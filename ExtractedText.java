9336034208aa8b7de35eb427e7f60f5fa0feec87
JahnviJain <138013346+JahnviJain@users.noreply.github.com>
2024-01-18 18:00:48 +0530

    commit

---
+++ ExtractedText.java
@@ -0,0 +1,17 @@
+public class ExtractedText {
+	private String text;
+    private String url;
+
+    public ExtractedText(String text, String url) {
+        this.text = text;
+        this.url = url;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
